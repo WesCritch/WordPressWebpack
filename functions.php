62beac8da21081dd8312905c006ca7337a33ea8e
WesCritch <wesley@wizbit.net>
2021-11-11 09:48:04 +0000

    Removed WP_Theme()

--- functions.php
+++ functions.php
@@ -1,25 +1,24 @@
 <?php
 
-   function isDev() {
-      if (preg_match('/\.local$/', $_SERVER['SERVER_NAME'])) {
-         echo 'yesss';
-         return true;
-      }
-      if (preg_match("/dev\.wizbit\.net/", $_SERVER['SERVER_NAME'])) {
-         return true;
-      }
-      echo 'no';
-      return false;
-
+function isDev() {
+   if (preg_match('/\.local$/', $_SERVER['SERVER_NAME'])) {
+      echo 'yesss';
+      return true;
    }
-
-   function css_styles() {
-      wp_enqueue_style('main-css', get_theme_file_uri('./css/main.css'));
-      if (isDev()) {
-         wp_enqueue_style('tailwind-dev', get_theme_file_uri('./css/tailwind.css'));
-      } else {
-         wp_enqueue_style('tailwind-min', get_theme_file_uri('./css/tailwind.min.css'));
-      }
+   if (preg_match("/dev\.wizbit\.net/", $_SERVER['SERVER_NAME'])) {
+      return true;
    }
+   echo 'no';
+   return false;
+}
 
-   add_action('wp_enqueue_scripts', 'css_styles');
+function css_styles() {
+   wp_enqueue_style('main-css', get_theme_file_uri('./css/main.css'));
+   if (isDev()) {
+      wp_enqueue_style('tailwind-dev', get_theme_file_uri('./css/tailwind.css'));
+   } else {
+      wp_enqueue_style('tailwind-min', get_theme_file_uri('./css/tailwind.min.css'));
+   }
+}
+
+add_action('wp_enqueue_scripts', 'css_styles');
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-   get_header();
+get_header();
 ?>
 <div>
    <h1>Hello</h1>
@@ -7,9 +7,27 @@
    <h2 class="text-4xl italic text-blue-500">hello</h2>
    <p class="font-semibold">Hi</p>
    <h2>hi</h2>
-   <p>highghghghghg</p>
+   <p>test</p>
+</div>
+
+<div>
+   <h1>Hello</h1>
+   <p>Peter</p>
+   <h2 class="text-4xl italic text-blue-500">hello</h2>
+   <p class="font-semibold">Hi</p>
+   <?php
+   $args = [
+      'posts_per_page' => 3,
+      'order' => 'ASC',
+   ];
+   $posts = new WP_Query($args);
+   while ($posts->have_posts()) : $posts->the_post();
+   ?>
+      <h2>hi</h2>
+      <p>test</p>
+   <?php endwhile; ?>
 </div>
 
 <?php
-   get_footer();
+get_footer();
 ?>
